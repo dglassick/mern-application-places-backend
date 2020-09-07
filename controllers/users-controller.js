66a418f15d6e6e84a4aec9e73a8d1783ebe547da
Derek Glassick <derek.glassick@gmail.com>
2020-09-07 12:00:57 -0700

    created places routes, get all users, create user, and login

---
+++ controllers/users-controller.js
@@ -0,0 +1,71 @@
+const { v4: uuidv4 } = require('uuid');
+
+const HttpError = require('../models/http-errors');
+
+let DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Derek Glassick',
+    email: 'test@test.com',
+    password: 'testers'
+  },
+  {
+    id: 'u2',
+    name: 'Ashley Glassick',
+    email: 'ashley@test.com',
+    password: 'ashley'
+  },
+  {
+    id: 'u3',
+    name: 'Jameson Glassick',
+    email: 'jameson@test.com',
+    password: 'jameson'
+  },
+  {
+    id: 'u4',
+    name: 'Teddy Glassick',
+    email: 'teddy@test.com',
+    password: 'theodore'
+  }
+];
+
+const getUsers = (req, res, next) => {
+  res.json({ users: DUMMY_USERS });
+};
+
+const signup = (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const hasUser = DUMMY_USERS.find(u => u.email === email);
+  if (hasUser) {
+    throw new HttpError('Could not create user, email already exists.', 422);
+  }
+
+  const createdUser = {
+    id: uuidv4,
+    name,
+    email,
+    password
+  };
+
+  DUMMY_USERS.push(createdUser);
+  res.status(201).json({ user: createdUser });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  const identifiedUser = DUMMY_USERS.find(u => u.email === email);
+  if (!identifiedUser || identifiedUser.password !== password) {
+    throw new HttpError(
+      'Could not identify user, credentials seem to be wrong.',
+      401
+    );
+  }
+
+  res.json({ message: 'Logged in!' });
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
